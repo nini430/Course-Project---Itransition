1188fe83b2ad446e90f35677e57d724454427a49
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-06-12 16:08:05 +0400

    fix

---
+++ server/dist/src/app.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const api_1 = __importDefault(require("./routes/api"));
+const errorHandler_1 = __importDefault(require("./middleware/errorHandler"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use('/api/v1', api_1.default);
+app.use(errorHandler_1.default);
+exports.default = app;
---
+++ server/dist/src/sockets/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ioListen = (io) => {
+    io.on('connection', () => {
+    });
+};
+exports.default = ioListen;
