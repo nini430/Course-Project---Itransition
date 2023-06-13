7e0d20668241ba6e617694a90193b3b49f647718
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-06-13 10:11:39 +0400

    fix server compilation

--- server/dist/src/utils/logger.js
+++ server/dist/src/utils/logger.js
@@ -4,14 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const pino_1 = __importDefault(require("pino"));
-const config_1 = __importDefault(require("config"));
 const dayjs_1 = __importDefault(require("dayjs"));
-const level = config_1.default.get('logLevel');
 const log = (0, pino_1.default)({
     transport: {
         target: 'pino-pretty'
     },
-    level,
+    level: 'info',
     base: {
         pid: false
     },
