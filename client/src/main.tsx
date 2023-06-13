a6f6a6973875b93c7495be3bfd5c24455f909265
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-06-13 07:40:50 +0400

    fx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+
 import App from './App.tsx';
 import store from './store/store.ts';
 import './index.css';
